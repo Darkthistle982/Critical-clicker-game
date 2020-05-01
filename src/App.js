6a3787fd47670591c448e2a83797abaa2c673ca6
Brian Allen <brianallen2401@gmail.com>
2020-05-01 13:24:54 -0600

    wrote main logic for game.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/NavBar';
 import Jumbotron from './components/pages/jumbotron/jumbotron';
+import Card from './components/pages/cards/cards';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Jumbotron />
+      <Card />
     </div>
   );
 }
--- src/components/pages/game.js
+++ src/components/pages/game.js
@@ -1,16 +1,75 @@
 import React, { Component } from "react";
-import Card from '../pages/cards/cards';
-
+import Card from "../pages/cards/cards";
+import images from "../../images.json";
+import { render } from "@testing-library/react";
 
 class Game extends Component {
-    state = {
-        score: 0
+  state = {
+    images,
+    message: "Click on of the pictures to begin!",
+    score: 0,
+    topScore: 0,
+  };
+
+  handleClick = (id, clicked) => {
+    const imageOrder = this.state.images;
+
+    if (clicked) {
+      imageOrder.forEach((image, index) => {
+        imageOrder[index].clicked = false;
+      });
+      return this.setState({
+        image: imageOrder.sort(() => Math.random() - 0.5),
+        message: "You guessed Incorrectly!",
+        score: 0,
+      });
+    } else {
+      imageOrder.forEach((image, index) => {
+        if (id === image.id) {
+          imageOrder[index].clicked = true;
+        }
+      });
+      const { topScore, score } = this.state;
+      const newScore = score + 1;
+      const newTopScore = newScore > topScore ? newScore : topScore;
+
+      return this.setState({
+        image: imageOrder.sort(() => Math.random() - 0.5),
+        message: "You guessed correctly!",
+        score: newScore,
+        topScore: newTopScore,
+      });
     }
+  };
 
-    // handleScore = (event) => {
-    //     this.setState({ score: this.state.score + 1})
-    // }
-
+  render() {
+    return (
+      <div className="container-fluid cardContainer">
+        <div className="gameMessage text-center">
+          <p>{this.state.message}</p>
+        </div>
+        <div className="gameScores text-center">
+          <p>
+            Score: {this.state.score} | Top Score: {this.state.topScore}
+          </p>
+        </div>
+        <div className="container">
+          <div className="row">
+            {this.state.images.map((image) => (
+              <Card
+                key={image.id}
+                id={image.id}
+                name={image.name}
+                clicked={image.clicked}
+                image={image.image}
+                handleClick={this.handleClick}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
