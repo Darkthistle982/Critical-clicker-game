07436a6c415c8c4b697f2bb059fd1ae6b03eaa2a
Brian Allen <brianallen2401@gmail.com>
2020-05-01 13:28:43 -0600

    added styling to cards and got them to render

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from './components/NavBar';
 import Jumbotron from './components/pages/jumbotron/jumbotron';
 import Card from './components/pages/cards/cards';
+import Game from './components/pages/game';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Jumbotron />
-      <Card />
+      <Game />
     </div>
   );
 }
